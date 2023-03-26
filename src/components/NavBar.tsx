b2c574da3b3f570f0da94b397c656c8db1f3d995
Felice Forgione <forgione67@gmail.com>
2023-03-26 01:51:33 -0400

    Color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justify="space-between" px="5px">
       <Image src={logo} boxSize="60px" />
-      <Text>CVS Pharmacy</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
