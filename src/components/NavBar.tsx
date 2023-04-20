c7d6af222e1862ad6bc282a5b8926fa052fe278c
Felice Forgione <forgione67@gmail.com>
2023-04-19 20:07:46 -0400

    Updated SearchInput component look

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack px="5px">
-      <Image src={logo} boxSize="100px" />
-      <SearchInput />
+    <HStack px="5px" justifyContent="space-between">
+      <Image src={logo} width="150px" p="20px" />
+      <Box px="20px" width="60%">
+        <SearchInput />
+      </Box>
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,31 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
 function SearchInput() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (ref.current) {
+      console.log(ref.current.value);
+    }
+  }
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} variant={"filled"} placeholder="Search..." />;
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup>
+        <InputLeftElement>
+          <BsSearch />
+        </InputLeftElement>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          variant={"filled"}
+          placeholder="Search..."
+        />
+        ;
+      </InputGroup>
+    </form>
   );
 }
 
