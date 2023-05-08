16e9786e699ec39ae040a6ed65f988f9b7360400
Felice Forgione <forgione67@gmail.com>
2023-05-08 01:35:28 -0400

    Added base url to env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
   params: {},
 });
 
+console.log(import.meta.env.VITE_API_URL);
 const getData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig) => {
   return apiClient
     .get(endpoint, { ...requestConfig })
